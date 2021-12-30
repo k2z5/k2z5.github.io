05fff59ce807a0872e87d5e716381456662142b9
k2z5 <k2z5@yandex.ru>
2021-12-30 18:20:21 +0300

    5
    5

---
+++ salestream/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://salestream.gremlin.cf/admin/');
+define('HTTP_CATALOG', 'http://salestream.gremlin.cf/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://salestream.gremlin.cf/admin/');
+define('HTTPS_CATALOG', 'http://salestream.gremlin.cf/');
+
+// DIR
+define('DIR_APPLICATION', '/home/smz11/domains/gremlin.cf/public_html/salestream/admin/');
+define('DIR_SYSTEM', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/');
+define('DIR_IMAGE', '/home/smz11/domains/gremlin.cf/public_html/salestream/image/');
+define('DIR_LANGUAGE', '/home/smz11/domains/gremlin.cf/public_html/salestream/admin/language/');
+define('DIR_TEMPLATE', '/home/smz11/domains/gremlin.cf/public_html/salestream/admin/view/template/');
+define('DIR_CONFIG', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/config/');
+define('DIR_CACHE', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/storage/download/');
+define('DIR_LOGS', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/storage/modification/');
+define('DIR_UPLOAD', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/storage/upload/');
+define('DIR_CATALOG', '/home/smz11/domains/gremlin.cf/public_html/salestream/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'smz11_salestream');
+define('DB_PASSWORD', 'unzTx29Z');
+define('DB_DATABASE', 'smz11_salestream');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ salestream/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://salestream.gremlin.cf/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://salestream.gremlin.cf/');
+
+// DIR
+define('DIR_APPLICATION', '/home/smz11/domains/gremlin.cf/public_html/salestream/catalog/');
+define('DIR_SYSTEM', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/');
+define('DIR_IMAGE', '/home/smz11/domains/gremlin.cf/public_html/salestream/image/');
+define('DIR_LANGUAGE', '/home/smz11/domains/gremlin.cf/public_html/salestream/catalog/language/');
+define('DIR_TEMPLATE', '/home/smz11/domains/gremlin.cf/public_html/salestream/catalog/view/theme/');
+define('DIR_CONFIG', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/config/');
+define('DIR_CACHE', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/storage/download/');
+define('DIR_LOGS', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/storage/modification/');
+define('DIR_UPLOAD', '/home/smz11/domains/gremlin.cf/public_html/salestream/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'smz11_salestream');
+define('DB_PASSWORD', 'unzTx29Z');
+define('DB_DATABASE', 'smz11_salestream');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
